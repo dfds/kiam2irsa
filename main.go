d22df222edaa1852f4b3d63b8dc3a889af640351
Audun Nes <aunes@dfds.com>
2022-04-26 14:41:18 +0200

    Refactor functions to separate package
    Signed-off-by: Audun Nes <aunes@dfds.com>

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"context"
 	"os"
 	"path/filepath"
 
+	"github.com/dfds/kiam2irsa/pkg/k8s/sa"
 	"go.uber.org/zap"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-)
-
-const (
-	ROLE_ARN_ANNOTATION_NAME      string = "eks.amazonaws.com/role-arn"
-	REGIONAL_STS_ANNOTATION_NAME  string = "eks.amazonaws.com/sts-regional-endpoints"
-	REGIONAL_STS_ANNOTATION_VALUE string = "true"
 )
 
 func main() {
@@ -26,8 +16,11 @@ func main() {
 		}
 	}(logger)
 	sugar := logger.Sugar()
-
-	home := homedir.HomeDir()
+	home, err := os.UserHomeDir()
+	if err != nil {
+		sugar.Error(err.Error())
+		return
+	}
 
 	// TODO: Replace this dirty hack with the cobra CLI
 	kubeconfig := filepath.Join(home, ".kube", "config")
@@ -36,37 +29,5 @@ func main() {
 		kubeconfig = args[1]
 	}
 
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	serviceAccounts, err := clientset.CoreV1().ServiceAccounts("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for _, sa := range serviceAccounts.Items {
-		name := sa.Name
-		ns := sa.Namespace
-		annotations := sa.Annotations
-		hasFavorable := false
-		hasUndesirable := false
-		for annoKey, annoValue := range annotations {
-			if annoKey == ROLE_ARN_ANNOTATION_NAME {
-				hasFavorable = true
-			}
-			if annoKey == REGIONAL_STS_ANNOTATION_NAME && annoValue == REGIONAL_STS_ANNOTATION_VALUE {
-				hasUndesirable = true
-			}
-		}
-		if hasFavorable && !hasUndesirable {
-			sugar.Infof("Service account %s in namespace %s is not yet migrated to IRSA", name, ns)
-		}
-	}
+	sa.GetSA(kubeconfig)
 }
---
+++ pkg/k8s/sa/annotations.go
@@ -0,0 +1,60 @@
+package sa
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	ROLE_ARN_ANNOTATION_NAME      string = "eks.amazonaws.com/role-arn"
+	REGIONAL_STS_ANNOTATION_NAME  string = "eks.amazonaws.com/sts-regional-endpoints"
+	REGIONAL_STS_ANNOTATION_VALUE string = "true"
+)
+
+func GetSA(kubeconfig string) {
+	logger, _ := zap.NewDevelopment()
+	defer func(logger *zap.Logger) {
+		err := logger.Sync()
+		if err != nil {
+		}
+	}(logger)
+	sugar := logger.Sugar()
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	serviceAccounts, err := clientset.CoreV1().ServiceAccounts("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, sa := range serviceAccounts.Items {
+		name := sa.Name
+		ns := sa.Namespace
+		annotations := sa.Annotations
+		hasFavorable := false
+		hasUndesirable := false
+		for annoKey, annoValue := range annotations {
+			if annoKey == ROLE_ARN_ANNOTATION_NAME {
+				hasFavorable = true
+			}
+			if annoKey == REGIONAL_STS_ANNOTATION_NAME && annoValue == REGIONAL_STS_ANNOTATION_VALUE {
+				hasUndesirable = true
+			}
+		}
+		if hasFavorable && !hasUndesirable {
+			sugar.Infof("Service account %s in namespace %s is not yet migrated to IRSA", name, ns)
+		}
+	}
+}
